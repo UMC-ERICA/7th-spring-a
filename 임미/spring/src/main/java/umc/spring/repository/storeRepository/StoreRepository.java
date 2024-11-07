6d241cfa7fe6596a7fe5d2d8f0310e2f24fc4bc0
전재연 <sunrox1107@gmail.com>
2024-11-07 12:53:30 +0900

    [feat] StoreQueryService 구현

--- 임미/spring/src/main/java/umc/spring/repository/storeRepository/StoreRepository.java
+++ 임미/spring/src/main/java/umc/spring/repository/storeRepository/StoreRepository.java
@@ -3,5 +3,5 @@ package umc.spring.repository.storeRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.store.Store;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
+public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
 }
