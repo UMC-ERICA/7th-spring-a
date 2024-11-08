5b010084032ac7542ff8ff963f1216f97dea4967
gdbs1107 <gdbs1107@naver.com>
2024-11-08 09:47:13 +0900

    [feat] reviewRepository 구현

--- 임미/spring/src/main/java/umc/spring/repository/memberMissionRepository/MemberMissionRepositoryImpl.java
+++ 임미/spring/src/main/java/umc/spring/repository/memberMissionRepository/MemberMissionRepositoryImpl.java
@@ -1,16 +1,30 @@
 package umc.spring.repository.memberMissionRepository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import umc.spring.domain.member.QMember;
+import umc.spring.domain.mission.MemberMission;
 import umc.spring.domain.mission.QMemberMission;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class MemberMissionRepositoryImpl implements MemberMissionRepository {
+public class MemberMissionRepositoryImpl implements MemberMissionRepositoryCustom {
 
-    QMemberMission memberMission = QMemberMission.memberMission;
-    QMember member = QMember.member;
+    private final JPAQueryFactory jpaQueryFactory;
 
+    private final QMemberMission memberMission = QMemberMission.memberMission;
+    private final QMember member = QMember.member;
 
+    public List<MemberMission> findRecentMemberMissions(int n) {
+        return jpaQueryFactory
+                .selectFrom(memberMission)
+                .join(memberMission.member, member)
+                .orderBy(memberMission.createdAt.desc())
+                .limit(3)
+                .offset((n - 1) * 3)
+                .fetch();
+    }
 }
---
+++ 임미/spring/src/main/java/umc/spring/repository/reviewRepository/ReviewRepositoryImpl.java
@@ -0,0 +1,52 @@
+package umc.spring.repository.reviewRepository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import umc.spring.domain.Review;
+import umc.spring.domain.member.Member;
+import umc.spring.domain.member.QMember;
+import umc.spring.domain.store.QStore;
+import umc.spring.domain.store.Store;
+import umc.spring.repository.MemberRepository;
+import umc.spring.repository.storeRepository.StoreRepository;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+
+    private final QMember member = QMember.member;
+
+    public void saveReview(String memberName, String storeName, String content, int score) {
+
+        Long memberId = queryFactory
+                .select(member.id)
+                .from(member)
+                .where(member.name.eq(memberName))
+                .fetchOne();
+
+        Member findMember = memberRepository.findById(memberId)
+                .orElseThrow(NoSuchElementException::new);
+
+
+        if (memberId != null) {
+
+            Review newReview = Review.builder()
+                            .content(content)
+                                    .score(score)
+                                            .member(findMember)
+                                                    .build();
+
+            reviewRepository.save(newReview);
+        } else {
+            throw new IllegalArgumentException("해당 멤버나 가게를 찾을 수 없습니다.");
+        }
+    }
+}
