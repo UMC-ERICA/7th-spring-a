d976758708c0f377fa24a358095629f0d32358fd
gdbs1107 <gdbs1107@naver.com>
2024-11-13 18:57:40 +0900

    [fix] ReviewRepository 순환참조 오류 발생 해결

--- 임미/spring/src/main/java/umc/spring/repository/reviewRepository/ReviewRepository.java
+++ 임미/spring/src/main/java/umc/spring/repository/reviewRepository/ReviewRepository.java
@@ -3,5 +3,5 @@ package umc.spring.repository.reviewRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>,ReviewRepositoryCustom {
 }
