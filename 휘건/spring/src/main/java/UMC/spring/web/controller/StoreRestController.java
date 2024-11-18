6a475884a6f69c3370b5770dab57bed5021804cd
Hwigun46 <hy010827@naver.com>
2024-11-19 02:03:28 +0900

    [FEAT] 가게 추가 API

--- 휘건/spring/src/main/java/UMC/spring/web/controller/StoreRestController.java
+++ 휘건/spring/src/main/java/UMC/spring/web/controller/StoreRestController.java
@@ -1,2 +1,29 @@
-package UMC.spring.web.controller;public class StoreRestController {
+package UMC.spring.web.controller;
+
+import UMC.spring.api.ApiResponse;
+import UMC.spring.converter.StoreConverter;
+import UMC.spring.domain.Store;
+import UMC.spring.service.storeService.StoreCommandService;
+import UMC.spring.web.dto.storeDTO.StoreRequestDTO;
+import UMC.spring.web.dto.storeDTO.StoreResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreRestController {
+
+    private final StoreCommandService storeCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<StoreResponseDTO.CreateStoreResultDTO> createStore(@RequestBody @Valid StoreRequestDTO.CreateStoreDTO request) {
+        Store store = storeCommandService.CreateStore(request);
+        return ApiResponse.onSuccess(StoreConverter.toCreateResultDTO(store));
+    }
 }
