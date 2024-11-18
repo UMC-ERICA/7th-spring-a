280ea7de6576d6295b95e9e854741a79b1f4571a
Hwigun46 <hy010827@naver.com>
2024-11-19 01:30:35 +0900

    [FEAT] foodcategory 변환 메소드

--- 휘건/spring/src/main/java/UMC/spring/domain/enums/FoodCategory.java
+++ 휘건/spring/src/main/java/UMC/spring/domain/enums/FoodCategory.java
@@ -1,5 +1,8 @@
 package UMC.spring.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum FoodCategory {
     한식,
     일식,
@@ -12,6 +15,14 @@ public enum FoodCategory {
     야식,
     패스트푸드,
     디저트,
-    아시안푸드
+    아시안푸드;
 
+
+    public static FoodCategory fromString(String value) {
+        try {
+            return FoodCategory.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("알맞지 않은 카테고리입니다. " + value);
+        }
+    }
 }
