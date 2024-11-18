b81d9414de03c6aef2a0925d111f9160a9a954f5
gdbs1107 <gdbs1107@naver.com>
2024-11-18 17:30:37 +0900

    [chore] 어노테이션으로 에러 감지 시, 에러 코드가 아닌 메세지를 반환하도록 수정

--- 임미/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
+++ 임미/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -28,7 +28,7 @@ public class CategoriesExistValidator implements ConstraintValidator<ExistCatego
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.getMessage().toString()).addConstraintViolation();
         }
 
         return isValid;
