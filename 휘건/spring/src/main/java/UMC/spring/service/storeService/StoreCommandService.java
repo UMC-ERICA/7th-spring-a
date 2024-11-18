7e5e92d53dd60ceddc86239e180d5836b7afd5f5
Hwigun46 <hy010827@naver.com>
2024-11-19 01:35:35 +0900

    [FEAT] storeService & storeServiceImpl

--- 휘건/spring/src/main/java/UMC/spring/service/storeService/StoreCommandService.java
+++ 휘건/spring/src/main/java/UMC/spring/service/storeService/StoreCommandService.java
@@ -1,4 +1,9 @@
 package UMC.spring.service.storeService;
 
+import UMC.spring.domain.Store;
+import UMC.spring.web.dto.storeDTO.StoreRequestDTO;
+
 public interface StoreCommandService {
+
+    Store CreateStore(StoreRequestDTO.CreateStoreDTO request);
 }
--- 휘건/spring/src/main/java/UMC/spring/service/storeService/StoreCommandServiceImpl.java
+++ 휘건/spring/src/main/java/UMC/spring/service/storeService/StoreCommandServiceImpl.java
@@ -1,9 +1,43 @@
 package UMC.spring.service.storeService;
 
+import UMC.spring.domain.Region;
+import UMC.spring.domain.Store;
+import UMC.spring.domain.enums.FoodCategory;
+import UMC.spring.repository.regionRepository.RegionRepository;
+import UMC.spring.repository.storeRepository.StoreRepository;
+import UMC.spring.web.dto.storeDTO.StoreRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class StoreCommandServiceImpl implements StoreCommandService{
+    private final StoreRepository storeRepository;
+    private final RegionRepository regionRepository;
+
+    @Override
+    public Store CreateStore(StoreRequestDTO.CreateStoreDTO request) {
+
+        Region region = regionRepository.findByCityAndDistrictAndNeighborhood(
+                request.getCity(),
+                request.getDistrict(),
+                request.getNeighborhood()
+        ).orElseThrow(() -> new IllegalArgumentException("Region not found"));
+
+        FoodCategory category = FoodCategory.fromString(request.getStoreCategory());
+
+        Store newStore = Store.builder()
+                .storeName(request.getName())
+                .storeAddress(request.getStoreAddress())
+                .storeCategory(category)
+                .storeRate(BigDecimal.ZERO)
+                .region(region)
+                .build();
+
+
+        return storeRepository.save(newStore);
+    }
 }
