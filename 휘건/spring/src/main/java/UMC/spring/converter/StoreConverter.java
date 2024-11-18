101dfa2df94ce1a4c2161efa0a57f1b6de61fb00
Hwigun46 <hy010827@naver.com>
2024-11-19 02:01:34 +0900

    [FEAT] StoreConverter 추가

--- 휘건/spring/src/main/java/UMC/spring/converter/StoreConverter.java
+++ 휘건/spring/src/main/java/UMC/spring/converter/StoreConverter.java
@@ -1,2 +1,34 @@
-package UMC.spring.converter;public class StoreConverter {
+package UMC.spring.converter;
+
+import UMC.spring.domain.Region;
+import UMC.spring.domain.Store;
+import UMC.spring.domain.enums.FoodCategory;
+import UMC.spring.web.dto.storeDTO.StoreRequestDTO;
+import UMC.spring.web.dto.storeDTO.StoreResponseDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class StoreConverter {
+
+    public static StoreResponseDTO.CreateStoreResultDTO toCreateResultDTO(Store store) {
+
+        return StoreResponseDTO.CreateStoreResultDTO.builder()
+                .storeId(store.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+
+    public static Store toStore(StoreRequestDTO.CreateStoreDTO request,FoodCategory category ,Region region) {
+
+
+        return  Store.builder()
+                .storeName(request.getName())
+                .storeAddress(request.getStoreAddress())
+                .storeCategory(category)
+                .storeRate(BigDecimal.ZERO)
+                .region(region)
+                .build();
+    }
 }
