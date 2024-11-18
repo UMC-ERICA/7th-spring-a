00540bd9b396e1f2ce42809bf65d34540589b428
Hwigun46 <hy010827@naver.com>
2024-11-19 00:48:08 +0900

    [FEAT] 가게등록 responseDTO

--- 휘건/spring/src/main/java/UMC/spring/web/dto/storeDTO/StoreResponseDTO.java
+++ 휘건/spring/src/main/java/UMC/spring/web/dto/storeDTO/StoreResponseDTO.java
@@ -1,4 +1,20 @@
 package UMC.spring.web.dto.storeDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class StoreResponseDTO {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class CreateStoreResultDTO {
+        Long storeId;
+        LocalDateTime createdAt;
+    }
 }
