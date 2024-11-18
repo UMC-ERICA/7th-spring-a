86a1cd5d56fd7708e7c9a324fc1264263bf35041
gdbs1107 <gdbs1107@naver.com>
2024-11-18 17:16:58 +0900

    [feat] ExistCategories 어노테이션 구현

---
+++ 임미/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,37 @@
+package umc.spring.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayLoad.code.status.ErrorStatus;
+import umc.spring.repository.foodCategoryRepository.FoodCategoryRepository;
+import umc.spring.validation.annotation.ExistCategories;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
+    }
+}
