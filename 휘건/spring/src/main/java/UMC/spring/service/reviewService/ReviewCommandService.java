b6c9494f1045ac92eaf39335f9f074bb8a173a7a
Hwigun46 <hy010827@naver.com>
2024-11-19 23:51:18 +0900

    [FEAT] reviewService 구현

--- 휘건/spring/src/main/java/UMC/spring/service/reviewService/ReviewCommandService.java
+++ 휘건/spring/src/main/java/UMC/spring/service/reviewService/ReviewCommandService.java
@@ -1,2 +1,9 @@
-package UMC.spring.service.reviewService;public interface ReviewCommandService {
+package UMC.spring.service.reviewService;
+
+import UMC.spring.domain.Review;
+import UMC.spring.web.dto.reviewDTO.ReviewRequestDTO;
+
+public interface ReviewCommandService {
+
+    Review CreateReview(ReviewRequestDTO.CreateReviewDTO request, Long storeId);
 }
--- 휘건/spring/src/main/java/UMC/spring/service/reviewService/ReviewCommandServiceImpl.java
+++ 휘건/spring/src/main/java/UMC/spring/service/reviewService/ReviewCommandServiceImpl.java
@@ -1,2 +1,41 @@
-package UMC.spring.service.reviewService;public class ReviewCommandServiceImpl {
+package UMC.spring.service.reviewService;
+
+import UMC.spring.api.code.status.ErrorStatus;
+import UMC.spring.api.exception.handler.MemberHandler;
+import UMC.spring.api.exception.handler.StoreHandler;
+import UMC.spring.converter.ReviewConverter;
+import UMC.spring.domain.Member;
+import UMC.spring.domain.Review;
+import UMC.spring.domain.Store;
+import UMC.spring.repository.memberRepository.MemberRepository;
+import UMC.spring.repository.reviewRepository.ReviewRepository;
+import UMC.spring.repository.storeRepository.StoreRepository;
+import UMC.spring.web.dto.reviewDTO.ReviewRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewCommandServiceImpl implements ReviewCommandService{
+
+    private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final StoreRepository storeRepository;
+
+    @Override
+    public Review CreateReview(ReviewRequestDTO.CreateReviewDTO request, Long storeId) {
+
+        Member member = memberRepository.findById(request.getMemberId())
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreHandler(ErrorStatus.STORE_NOT_FOUND));
+
+        Review review = ReviewConverter.toReview(request, member, store);
+
+        store.updateStoreRate(request.getRate());
+        storeRepository.save(store);
+
+        return review;
+    }
 }
