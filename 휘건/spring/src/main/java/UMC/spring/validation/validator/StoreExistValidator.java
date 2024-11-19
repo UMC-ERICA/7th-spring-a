235a92bc26404669e24f09b884a080d1666f8157
Hwigun46 <hy010827@naver.com>
2024-11-20 03:33:12 +0900

    [FEAT] store custom validation 구현

--- 휘건/spring/src/main/java/UMC/spring/validation/validator/StoreExistValidator.java
+++ 휘건/spring/src/main/java/UMC/spring/validation/validator/StoreExistValidator.java
@@ -1,2 +1,32 @@
-package UMC.spring.validation.validator;public class StoresExistValidator {
+package UMC.spring.validation.validator;
+
+import UMC.spring.api.code.status.ErrorStatus;
+import UMC.spring.service.storeService.StoreCommandService;
+import UMC.spring.validation.annotation.ExistStore;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StoreExistValidator implements ConstraintValidator<ExistStore, Long> {
+
+    private final StoreCommandService storeCommandService;
+
+    @Override
+    public void initialize(ExistStore constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long value, ConstraintValidatorContext context) {
+        boolean isValid = storeCommandService.isStoreExist(value);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.STORE_NOT_FOUND.toString()).addConstraintViolation();
+        }
+        return isValid;
+    }
 }
