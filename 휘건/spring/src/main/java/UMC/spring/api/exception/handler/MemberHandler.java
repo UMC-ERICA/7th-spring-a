d352a3a016eac2e57cc34a2ae1164cbf08de864d
Hwigun46 <hy010827@naver.com>
2024-11-19 23:28:24 +0900

    [FEAT] memberHandler 추가

--- 휘건/spring/src/main/java/UMC/spring/api/exception/handler/MemberHandler.java
+++ 휘건/spring/src/main/java/UMC/spring/api/exception/handler/MemberHandler.java
@@ -1,2 +1,11 @@
-package UMC.spring.api.exception.handler;public class MemberHandler {
+package UMC.spring.api.exception.handler;
+
+import UMC.spring.api.code.BaseErrorCode;
+import UMC.spring.api.exception.GeneralException;
+
+public class MemberHandler extends GeneralException {
+
+    public MemberHandler(BaseErrorCode errorCode) {
+        super(errorCode);
+    }
 }
