1420c5c4d3efe1c917435f9d1f1802d8f3bfb788
Hwigun46 <hy010827@naver.com>
2024-11-19 23:28:59 +0900

    [FEAT] 가게 관련 에러 추가

--- 휘건/spring/src/main/java/UMC/spring/api/code/status/ErrorStatus.java
+++ 휘건/spring/src/main/java/UMC/spring/api/code/status/ErrorStatus.java
@@ -20,6 +20,9 @@ public enum ErrorStatus implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
     NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수입니다."),
 
+    // 가게 관련 에러
+    STORE_NOT_FOUND(HttpStatus.BAD_REQUEST, "STORE4001", "가게가 없습니다."),
+
 
     //음식 카테고리 에러
     FOOD_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "PREFER4001", "알맞지 않은 음식 카테고리입니다."),
