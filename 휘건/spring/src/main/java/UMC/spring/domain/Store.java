5335a0f1327c2bc299cfd3f9e617b17e2e8195a2
Hwigun46 <hy010827@naver.com>
2024-11-19 23:49:35 +0900

    [FEAT] 가게 평점 계산 메소드 추가 및 필드 추가

--- 휘건/spring/src/main/java/UMC/spring/domain/Store.java
+++ 휘건/spring/src/main/java/UMC/spring/domain/Store.java
@@ -5,14 +5,19 @@ import UMC.spring.domain.enums.FoodCategory;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
 @Getter
 @Builder
 public class Store extends BaseEntity {
@@ -30,6 +35,12 @@ public class Store extends BaseEntity {
     @Column(precision = 3, scale = 2)
     @ColumnDefault("0.0")
     private BigDecimal storeRate;
+
+    @ColumnDefault("0")
+    private BigDecimal totalRate;
+
+    @ColumnDefault("0")
+    private Integer reviewCount;
 
     @Enumerated(EnumType.STRING)
     private FoodCategory storeCategory;
@@ -59,5 +70,24 @@ public class Store extends BaseEntity {
                 ", address='" + storeAddress + '\'' +
                 ", score=" + storeRate +
                 '}';
+    }
+
+    public void updateStoreRate(BigDecimal newReviewRate) {
+
+        this.totalRate = this.totalRate.add(newReviewRate);
+
+        this.reviewCount ++;
+
+        this.storeRate = this.totalRate.divide(BigDecimal.valueOf(this.reviewCount), 2, RoundingMode.HALF_UP);
+    }
+
+    public void removeReviewRate(BigDecimal reviewRate) {
+        this.totalRate = this.totalRate.subtract(reviewRate);
+
+        this.reviewCount --;
+
+        this.storeRate = this.reviewCount > 0
+                ? this.totalRate.divide(BigDecimal.valueOf(this.reviewCount), 2, RoundingMode.HALF_UP)
+                : BigDecimal.ZERO;
     }
 }
