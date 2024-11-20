993cd920e905a61de4872b7761a175f47725eb05
Hwigun46 <hy010827@naver.com>
2024-11-20 23:05:03 +0900

    [FEAT] missionRepository 추가

--- 휘건/spring/src/main/java/UMC/spring/repository/missionRepository/MissionRepository.java
+++ 휘건/spring/src/main/java/UMC/spring/repository/missionRepository/MissionRepository.java
@@ -1,2 +1,7 @@
-package UMC.spring.repository.missionRepository;public interface MissionRepository {
+package UMC.spring.repository.missionRepository;
+
+import UMC.spring.domain.Mission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
 }
