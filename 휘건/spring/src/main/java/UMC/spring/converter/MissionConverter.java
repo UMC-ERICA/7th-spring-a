478b35dad74fc3ba5bfdb23b2acbe037de1695a3
Hwigun46 <hy010827@naver.com>
2024-11-20 23:02:16 +0900

    [FEAT] missionconverter 구현

--- 휘건/spring/src/main/java/UMC/spring/converter/MissionConverter.java
+++ 휘건/spring/src/main/java/UMC/spring/converter/MissionConverter.java
@@ -1,2 +1,28 @@
-package UMC.spring.converter;public class MissionConverter {
+package UMC.spring.converter;
+
+import UMC.spring.domain.Mission;
+import UMC.spring.domain.Store;
+import UMC.spring.web.dto.missionDTO.MissionRequestDTO;
+import UMC.spring.web.dto.missionDTO.MissionResponseDTO;
+
+import java.time.LocalDateTime;
+
+public class MissionConverter {
+
+    public static MissionResponseDTO.CreateMissionResultDTO toCreateMissionResultDTO(Mission mission) {
+
+        return MissionResponseDTO.CreateMissionResultDTO.builder()
+                .missionId(mission.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Mission toMission(MissionRequestDTO.CreateMissionDTO request, Store store) {
+
+        return Mission.builder()
+                .content(request.getContent())
+                .rewardPoint(request.getReward())
+                .store(store)
+                .build();
+    }
 }
