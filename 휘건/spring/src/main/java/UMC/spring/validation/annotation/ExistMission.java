fe6aaa42fcfd8c2934116f5ac321d7658f1467ad
Hwigun46 <hy010827@naver.com>
2024-11-21 16:02:30 +0900

    [FEAT] Mission 커스텀 검증 구현

---
+++ 휘건/spring/src/main/java/UMC/spring/validation/annotation/ExistMission.java
@@ -0,0 +1,19 @@
+package UMC.spring.validation.annotation;
+
+import UMC.spring.validation.validator.MissionExistValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MissionExistValidator.class)
+@Target( {ElementType.METHOD,ElementType.FIELD,ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistMission {
+
+    String message() default "해당하는 미션이 .";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String storeIdField() default "storeId";
+}
---
+++ 휘건/spring/src/main/java/UMC/spring/validation/validator/MissionExistValidator.java
@@ -0,0 +1,32 @@
+package UMC.spring.validation.validator;
+
+import UMC.spring.api.code.status.ErrorStatus;
+import UMC.spring.service.missionService.MissionCommandService;
+import UMC.spring.validation.annotation.ExistMission;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MissionExistValidator implements ConstraintValidator<ExistMission, Long> {
+    private final MissionCommandService missionCommandService;
+
+    @Override
+    public void initialize(ExistMission constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long value, ConstraintValidatorContext context) {
+
+        boolean isValid = missionCommandService.isMissionExist(value);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.MISSION_NOT_FOUND.toString()).addConstraintViolation();
+        }
+        return isValid;
+    }
+}
