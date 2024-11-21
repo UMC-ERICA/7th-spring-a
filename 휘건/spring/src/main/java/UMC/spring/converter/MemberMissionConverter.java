f1f85af428ad96119f31b7e5cec253a829cdd83a
Hwigun46 <hy010827@naver.com>
2024-11-21 16:03:13 +0900

    [FEAT] MemberMissionConverter구현

---
+++ 휘건/spring/src/main/java/UMC/spring/converter/MemberMissionConverter.java
@@ -0,0 +1,31 @@
+package UMC.spring.converter;
+
+import UMC.spring.domain.mapping.MemberMission;
+import UMC.spring.web.dto.memberMissionDTO.MemberMissionResponseDTO;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@RequiredArgsConstructor
+public class MemberMissionConverter {
+
+
+    public static MemberMissionResponseDTO.CreateMemberMissionResultDTO toCreateMemberMissionResultDTO(MemberMission memberMission) {
+        return MemberMissionResponseDTO.CreateMemberMissionResultDTO.builder()
+                .memberMissionId(memberMission.getId())
+                .memberId(memberMission.getId())
+                .storeId(memberMission.getMission().getStore().getId())
+                .storeCategory(memberMission.getMission().getStore().getStoreCategory().toString())
+                .missionId(memberMission.getMission().getId())
+                .content(memberMission.getMission().getContent())
+                .reward(memberMission.getMission().getRewardPoint())
+                .missionStatus(memberMission.getMissionStatus().toString())
+                .expirationDate(memberMission.getMission().getExpirationDate())
+                .createdAt(memberMission.getCreatedAt())
+                .build();
+    }
+
+
+}
+
