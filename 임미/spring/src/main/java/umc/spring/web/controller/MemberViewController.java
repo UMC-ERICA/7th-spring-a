73cc8b4bebdc300e3866a677508746d3bed66ed8
gdbs1107 <gdbs1107@naver.com>
2024-11-29 13:32:58 +0900

    [feat] MemberVoewController 구현

---
+++ 임미/spring/src/main/java/umc/spring/web/controller/MemberViewController.java
@@ -0,0 +1,31 @@
+package umc.spring.web.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import umc.spring.web.dto.MemberRequestDTO;
+
+@Controller
+public class MemberViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+        model.addAttribute("memberJoinDto", new MemberRequestDTO.JoinDto());
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
